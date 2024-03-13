7f76033e9749acb691fd6a6d1d85be3f24bb101e
NAKAJIMA Yusaku <yupong7@gmail.com>
2024-03-13 15:19:11 +0900

    refactor

--- t/test.sh
+++ t/test.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-function error() {
-    echo "Error: $@" >&2
-    exit 1
-}
-
 cd ${0%/*}
 
 # test for Text(ASCII, UTF-8), binary file
 for script in char.pl line.pl file.pl; do
     for src in text.ascii text.utf8 Perl-camel-small.png ; do
-        cmp $src <(cat $src | perl ../$script) || error "script: $script, src: $src"
+        if ! cmp $src <(cat $src | perl ../$script); then 
+            echo "script: $script, src: $src" >&2
+            exit 1
+        fi
     done
 done
 
